ae7414a8ef5d0b4a596df9b7d375bb479490f4f5
Nastaran Dejagah <nastaran.dejagah@gmail.com>
2024-01-22 21:40:54 +0100

    CouponApi

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.CouponAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace Mango.Services.CouponAPI.Model
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAccount { get; set; }
+    }
+}
